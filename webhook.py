5a5a3836ae8dab8354c7c72faddd7b2494948b0f
Bryan Chu <thisisbryanchu@gmail.com>
2017-12-02 12:41:31 -0500

    Adding base webhook code

---
+++ webhook.py
@@ -0,0 +1,46 @@
+from bottle import route, run, request, response
+import json
+
+# Constants
+OBJECT = 'object'
+PAGE_OBJECT = 'page'
+ENTRY = 'entry'
+MESSAGING = 'messaging'
+MESSAGE = 'message'
+
+MODE = 'hub.mode'
+TOKEN = 'hub.verify_token'
+CHALLENGE = 'hub.challenge'
+SUBSCRIBE = 'subscribe'
+VERIFY_TOKEN = '<YOUR_VERIFY_TOKEN>'
+
+
+@route('/webhook', method='POST')
+def webhook_event():
+    req = json.load(request.body)
+
+    if req[OBJECT] == PAGE_OBJECT:
+        for entry in req[ENTRY]:
+            print(entry[MESSAGING][0][MESSAGE])
+            response.status = 200
+            return 'EVENT_RECEIVED'
+    else:
+        response.status = 404
+
+
+@route('/webhook', method='GET')
+def webhook_verify():
+    mode = request.query[MODE]
+    token = request.query[TOKEN]
+    challenge = request.query[CHALLENGE]
+
+    if mode and token:
+        if mode == SUBSCRIBE and token == VERIFY_TOKEN:
+            print('WEBHOOK_VERIFIED')
+            response.status = 200
+            return challenge
+        else:
+            response.status = 403
+
+
+run(host='localhost', port=8080)
