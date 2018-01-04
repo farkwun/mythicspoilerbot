3aded901029d16aa25b5f9331857e788d851b52e
Bryan Chu <thisisbryanchu@gmail.com>
2018-01-04 04:40:00 +0000

    Adding basic reply functionality

--- webhook.py
+++ webhook.py
@@ -1,6 +1,8 @@
 from bottle import route, run, request, response, default_app
 import json
 import msbot.mslib
+import requests
+
 import msbot.settings
 
 # Constants
@@ -9,6 +11,12 @@ PAGE_OBJECT = 'page'
 ENTRY = 'entry'
 MESSAGING = 'messaging'
 MESSAGE = 'message'
+SENDER = 'sender'
+ID = 'id'
+POSTBACK = 'postback'
+TEXT = 'text'
+RECIPIENT = 'recipient'
+ACCESS_TOKEN = 'access_token'
 
 MODE = 'hub.mode'
 TOKEN = 'hub.verify_token'
@@ -16,14 +24,34 @@ CHALLENGE = 'hub.challenge'
 SUBSCRIBE = 'subscribe'
 
 # Helpers
+def call_send_API(sender_psid, response):
+    request_body = {
+        RECIPIENT: {
+            ID: sender_psid
+        },
+        MESSAGE: response,
+    }
+
+    fb_url = "https://graph.facebook.com/v2.11/me/messages"
+
+    params = {
+        ACCESS_TOKEN: msbot.settings.PAGE_ACCESS_TOKEN,
+        RECIPIENT: sender_psid
+    }
+
+    r = requests.post(fb_url, params=params, json=request_body)
+
 def handle_message(sender_psid, received_message):
-    pass
+    if received_message[TEXT]:
+        response = {
+            TEXT: "You sent the message '%s'. Now send me an image!" %
+            received_message[TEXT]
+        }
+    call_send_API(sender_psid, response)
 
 def handle_postback(sender_psid, received_postback):
     pass
 
-def call_send_API(sender_psid, response):
-    pass
 
 @route('/webhook', method='POST')
 def webhook_event():
@@ -31,9 +59,15 @@ def webhook_event():
 
     if req[OBJECT] == PAGE_OBJECT:
         for entry in req[ENTRY]:
-            print(entry[MESSAGING][0][MESSAGE])
-            response.status = 200
-            return 'EVENT_RECEIVED'
+            event = entry[MESSAGING][0]
+            sender_psid = event[SENDER][ID]
+
+            if event[MESSAGE]:
+                handle_message(sender_psid, event[MESSAGE])
+            elif event[POSTBACK]:
+                handle_postback(sender_psid, event[POSTBACK])
+        response.status = 200
+        return 'EVENT_RECEIVED'
     else:
         response.status = 404
 
@@ -51,7 +85,6 @@ def webhook_verify():
             return challenge
         else:
             response.status = 403
-
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
