2c9469822d13c68bf06d1ea9067ad1abdd2ddbad
Bryan Chu <thisisbryanchu@gmail.com>
2018-09-24 14:31:26 -0400

    Fixes settings error and reformats mslib

--- msbot/mslib.py
+++ msbot/mslib.py
@@ -1,47 +1,48 @@
-import requests
 import json
+import requests
 import msbot.settings
 from msbot.spoiler import Spoiler
-import sqlite3
 
 url_base = 'http://mythicspoilerapi.dungeonmastering.net/'
 
+
 def getCardsBySet(setname):
-	"""
-	:type setname: String
-	:rtype: List[Spoiler]
-	"""
-	url = url_base + 'APIv2/cards/by/set'
-	payload = {'key': settings.API_KEY, 'param': setname}
-	r = requests.get(url, params = payload)
-	cards = json.loads(r.text[1:len(r.text)-1])['item']
-	output = ''
-	spoiler_list = []
-	for card in cards:
-		spoiler_list.append(Spoiler(card))
-	return spoiler_list
+    """
+    :type setname: String
+    :rtype: List[Spoiler]
+    """
+    url = url_base + 'APIv2/cards/by/set'
+    payload = {'key': msbot.settings.API_KEY, 'param': setname}
+    r = requests.get(url, params=payload)
+    cards = json.loads(r.text[1:len(r.text)-1])['item']
+    output = ''
+    spoiler_list = []
+    for card in cards:
+        spoiler_list.append(Spoiler(card))
+    return spoiler_list
+
 
 def getLatestSpoilers():
-	"""
-	:rtype: List[string]
-	"""
-	url = url_base + 'APIv2/cards/by/spoils'
-	payload = {'key': settings.API_KEY}
-	try:
-		r = requests.get(url, params = payload)
-	except Exception as e:
-		print('MythicSpoiler Connection Error')
-	else:
-		try:
-			print(r.text)
-			cards = json.loads(r.text[1:len(r.text)-1])['item']
-		except ValueError:
-			print('JSON error')
-			return []
-		else:
-			output = ''
-			url_list = []
-			for card in cards:
-				url_list.append(url_base + 'card_images/new_spoils/' + card['cardUrl'])
-			return url_list
-
+    """
+    :rtype: List[string]
+    """
+    url = url_base + 'APIv2/cards/by/spoils'
+    payload = {'key': msbot.settings.API_KEY}
+    try:
+        r = requests.get(url, params=payload)
+    except Exception as e:
+        print('MythicSpoiler Connection Error')
+    else:
+        try:
+            print(r.text)
+            cards = json.loads(r.text[1:len(r.text)-1])['item']
+        except ValueError:
+            print('JSON error')
+            return []
+        else:
+            output = ''
+            url_list = []
+            for card in cards:
+                url_list.append(
+                    url_base + 'card_images/new_spoils/' + card['cardUrl'])
+            return url_list
