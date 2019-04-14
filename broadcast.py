db4f36c31a1b4df0375a1f72bff72464420462b8
farkwun <thisisbryanchu@gmail.com>
2019-04-14 04:57:27 -0400

    Adds broadcast script for future use

---
+++ broadcast.py
@@ -0,0 +1,14 @@
+import webhook
+import msbot.msdb
+import msbot.constants
+
+db_file = 'db/broadcast_table.db'
+
+database = msbot.msdb.MSDatabase(db_file)
+
+broadcast_users = database.get_all_user_ids()
+
+message = 'Hello World!'
+
+for (user,) in broadcast_users:
+    webhook.send_message(user, { msbot.constants.TEXT: message })
