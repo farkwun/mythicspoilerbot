4233bf87c90aa4562adfa7b8db16785def9d3827
farkwun <thisisbryanchu@gmail.com>
2019-04-15 21:36:26 -0400

    Updates Spoiler object to serialize db rows

--- msbot/spoiler.py
+++ msbot/spoiler.py
@@ -1,13 +1,12 @@
 class Spoiler:
-	def __init__(self, json_input):
-		self.name = json_input['name']
-		self.cost = json_input['manacost']
-		self.type = json_input['type']
-		self.rarity = json_input['rarity']
-		self.text = json_input['text']
-		self.flavor = json_input['flavor']
-		self.artist = json_input['artist']
-		self.power = json_input['power']
-		self.toughness = json_input['toughness']
-		self.loyalty = json_input['loyalty']
+    def __init__(self, row_tuple):
+        print(row_tuple)
+        (self.image_url, self.attach_id, self.row_id) = row_tuple
 
+    def __repr__(self):
+        return ("<Spoiler object - img: {image}, "
+                "attach_id: {attach_id}, row_id: {row_id}>").format(
+                    image=self.image_url,
+                    attach_id=self.attach_id,
+                    row_id=self.row_id
+                )
