beb8af479345c2781f8d8897f6d9802b3041c5ff
farkwun <thisisbryanchu@gmail.com>
2019-04-15 23:59:31 -0400

    Updates spoiler attribute naming

--- msbot/spoiler.py
+++ msbot/spoiler.py
@@ -1,12 +1,12 @@
 class Spoiler:
     def __init__(self, row_tuple):
         print(row_tuple)
-        (self.image_url, self.attach_id, self.row_id) = row_tuple
+        (self.image_url, self.attach_id, self.spoiler_id) = row_tuple
 
     def __repr__(self):
         return ("<Spoiler object - img: {image}, "
-                "attach_id: {attach_id}, row_id: {row_id}>").format(
+                "attach_id: {attach_id}, row_id: {spoiler_id}>").format(
                     image=self.image_url,
                     attach_id=self.attach_id,
-                    row_id=self.row_id
+                    spoiler_id=self.spoiler_id
                 )
